8d8d112fffc43451df922ce1a7113cb82900a425
Daniel J. Summers <daniel.summers.2415@gmail.com>
2017-10-23 22:04:34 -0500

    Updated front page text (#12)

--- src/app/src/components/Home.vue
+++ src/app/src/components/Home.vue
@@ -5,12 +5,12 @@ article
   p &nbsp;
   p.
     myPrayerJournal is a place where individuals can record their prayer requests, record that they prayed for them,
-    update them as God moves in the situation, and record a final answer received on that request.&nbsp; It will also
-    allow individuals to review their answered prayers.
+    update them as God moves in the situation, and record a final answer received on that request. It will also allow
+    individuals to review their answered prayers.
   p.
-    This site is currently in very limited alpha, as it is being developed with a core group of test users.&nbsp; If
-    this is something in which you are interested, check back around mid-November 2017 for an update on the
-    development progress.
+    This site is currently in beta, but it is open and available to the general public. To get started, simply click
+    the "Log On" link above, and log on with either a Microsoft or Google account. You can also learn more about the
+    site at the "Docs" link, also above.
 </template>
 
 <script>
