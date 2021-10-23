b4c26bb6a9324eb6ec470f19d8f52156ccec15a9
Daniel J. Summers <daniel@bitbadger.solutions>
2021-10-22 23:43:39 -0400

    Remove Angular attempt

--- build.ps1
+++
@@ -1,8 +0,0 @@
-#!/snap/bin/pwsh
-Set-Location ./src/my-prayer-journal
-npm run build
-if ($?) {
-  Copy-Item ./dist/my-prayer-journal/* ../MyPrayerJournal.Api/wwwroot
-  Set-Location ../MyPrayerJournal.Api
-  dotnet run
-}
