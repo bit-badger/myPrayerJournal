98e5403da7a09b4adaa0623a539e3eaf558b8ba9
Daniel J. Summers <daniel@bitbadger.solutions>
2021-10-22 23:45:23 -0400

    Remove Vue 3 attempt

--- src/MyPrayerJournal/App/src/auth/auth0-variables.ts
+++
@@ -1,8 +0,0 @@
-
-export default {
-  clientId: 'Of2s0RQCQ3mt3dwIkOBY5h85J9sXbF2n',
-  domain: 'djs-consulting.auth0.com',
-  appDomain: `${location.protocol}//${location.host}`,
-  callbackUrl: '/user/log-on',
-  audience: 'https://prayerjournal.me'
-}
--- src/MyPrayerJournal/App/src/main.ts
+++
@@ -1,7 +0,0 @@
-import { createApp } from "vue"
-import App from "./App.vue"
-import auth, { key as authKey } from "./plugins/auth"
-import router from "./router"
-import store, { key as storeKey } from "./store"
-
-createApp(App).use(store, storeKey).use(router).use(auth, authKey).mount('#app')
